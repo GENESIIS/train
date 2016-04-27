730b6c840859a0c9521209d3aef56505dca4b325
pabodhaW <pabodha@genesiis.com>
2016-04-27 22:53:05 +0530

    HRA -9 SqlServerconnectionManager.java Class created.

--- hra_employer.jar/src/com/genesiis/hra/dbconnection/SqlServerConnectionManager.java
+++ hra_employer.jar/src/com/genesiis/hra/dbconnection/SqlServerConnectionManager.java
@@ -4,6 +4,11 @@ import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
 
+///***********************************************
+//* 20160427 PN HRA-9 created SqlServerConnectionManager.java class
+//*
+//***********************************************/
+
 public final class SqlServerConnectionManager {
 	private static final String DB_JNDI_NAME = "java:/hraDatabase";
 	private static DataSource dataSource;
