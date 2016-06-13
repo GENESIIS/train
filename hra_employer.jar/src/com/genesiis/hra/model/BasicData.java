86ee08f7a0713a61126da7411cc3199a24021758
prabathc <prabath@genesiis.com>
2016-06-13 12:06:48 +0530

    Corrcted error

--- hra_employer.jar/src/com/genesiis/hra/model/BasicData.java
+++ hra_employer.jar/src/com/genesiis/hra/model/BasicData.java
@@ -238,7 +238,7 @@ public class BasicData extends Employee {
 	public int update(Object employee, String epf) {
 		String query = "UPDATE [dbo].[HRA.EMPLOYEE] SET NAME = ? ,  DESIGNATION = ? , "
 				+ "  EMAIL = ? ,  DOB = ? ,  NIC = ?,  GENDER = ?,  PERMENENTADDRESS = ?, TEMPORARYADDRESS = ?, "
-				+ "  MOBILENO = ?,  OTHERNO = ?,  DEPTID = ?,  MARITALSTATUS = ?,  DATEOFJOIN = ?,  MODBY = ?,  EPF = ?,  BASIS  = ? WHERE EPF = ?";
+				+ "  MOBILENO = ?,  OTHERNO = ?,  DEPTID = ?,  MARITALSTATUS = ?,  DATEOFJOIN = ?,  MODBY = ?,MODON = GETDATE(),  EPF = ?,  BASIS  = ? WHERE EPF = ?";
 		String message = "Error";
 		Connection conn = null;
 		PreparedStatement ps = null;
@@ -256,13 +256,11 @@ public class BasicData extends Employee {
 			ps.setString(6, empBasic.getEmployeegender());
 			ps.setString(7, empBasic.getEmployeepermenetaddress());
 			ps.setString(8, empBasic.getEmployeetemporaryaddress());
-			ps.setString(7, empBasic.getEmployeepermenetaddress());
-			ps.setString(8, empBasic.getEmployeetemporaryaddress());
 			ps.setString(9, empBasic.getEmployeemobile());
 			ps.setString(10, empBasic.getEmployeetelephone());
-			ps.setString(11, empBasic.getEmployeedepartment());
+			ps.setString(11,  empBasic.getEmployeedepartment());
 			ps.setString(12, empBasic.getEmployeemaritalstatus());
-			ps.setString(13, empBasic.getEmployeejoindate());
+			ps.setString(13,  empBasic.getEmployeejoindate());
 			ps.setString(14, "S");
 			ps.setString(15, empBasic.getEmployeeepf());
 			ps.setString(16, empBasic.getEmployeebasis());
