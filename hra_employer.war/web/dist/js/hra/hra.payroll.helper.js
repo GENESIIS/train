9fa7a5817cdfe60cf25f8500f4a2677bc461e8f9
prabathc <prabath@genesiis.com>
2016-06-17 09:53:48 +0530

    srch component Done

--- hra_employer.war/web/dist/js/hra/hra.payroll.helper.js
+++ hra_employer.war/web/dist/js/hra/hra.payroll.helper.js
@@ -116,16 +116,7 @@ function addSalaryscheme() {
 
 	var salarySchemetitleerror = $("#salarySchemetitleerror").text();
 	var salaryCriteriaerror = $("#salaryCriteriaerror").text();
-	
-	/*var table = document.getElementById("mytab1");
-	for (var i = 0, row; row = table.rows[i]; i++) {
-	   //iterate through rows
-	   //rows would be accessed using the "row" variable assigned in the for loop
-	   for (var j = 0, col; col = row.cells[j]; j++) {
-	     //iterate through columns
-	     //columns would be accessed using the "col" variable assigned in the for loop
-	   }  
-	}*/
+		
 
 	var data = {
 		"title" : salarySchemetitle,
